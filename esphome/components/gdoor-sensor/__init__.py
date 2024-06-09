93b5a7ff00332c34f27c4382038868787f953558
Niklas Holloh <niklas.holloh@adesso-mobile.de>
2024-06-09 16:42:39 +0200

    main: init Fix 3?

--- esphome/components/gdoor-sensor/__init__.py
+++ esphome/components/gdoor-sensor/__init__.py
@@ -12,11 +12,9 @@ GDoorSensor = gdoor_esphome_ns.class_('GDoorSensor', text_sensor.TextSensor, cg.
 
 CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(GDoorSensor)
-}).extend(cv.COMPONENT_SCHEMA).extend(cv.only_with_arduino())
+}).extend(cv.COMPONENT_SCHEMA).extend(text_sensor.TEXT_SENSOR_SCHEMA).extend(cv.only_with_arduino())
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield text_sensor.register_text_sensor(var, config)
     yield cg.register_component(var, config)
-
-text_sensor.TextSensor.register_platform('gdoor_sensor', CONFIG_SCHEMA, to_code)
