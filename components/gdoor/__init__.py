f334c210868aa7fd5ea81b33e60b30c26e6dba83
Niklas Holloh <niklas.holloh@adesso-mobile.de>
2024-07-12 23:04:35 +0200

    test-folder-move: .

--- components/gdoor/__init__.py
+++ components/gdoor/__init__.py
@@ -1,6 +1,9 @@
+import re
 import esphome.codegen as cg
 import esphome.config_validation as cv
-import conf_constants as cid
+
+from esphome import pins
+from esphome.components import binary_sensor, button
 from esphome.const import (
   CONF_ID,
   CONF_RX_PIN,
@@ -8,26 +11,33 @@ from esphome.const import (
 )  
 
 MULTI_CONF = True
-CONF_GDOOR_ID = cid.CONF_GDOOR_ID
+AUTO_LOAD = []
 
-AUTO_LOAD = ["button", "text_sensor", "event"]
+gdoor_ns = cg.esphome_ns.namespace('gdoor_esphome')
+Gdoor = gdoor_ns.class_("GDoor", cg.Component)
+CONF_GDOOR = "gdoor"
 
-gdoor_esphome_ns = cg.esphome_ns.namespace('gdoor_esphome')
-GDoor = gdoor_esphome_ns.class_('GDoor', cg.Component)
+CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(Gdoor),
+        cv.Optional(CONF_RX_PIN): pins.gpio_input_pin_schema,
+        cv.Optional(CONF_SENSITIVITY, default=1.65): cv.float_range(min=1.3, max=1.65),
+    }
+)
 
-GDoorBusMessageButton_P = gdoor_esphome_ns.class_('GDoorBusMessageButton_P', cg.Parented.template(GDoor))
-GDoorLastMessageBusSensor_P = gdoor_esphome_ns.class_('GDoorLastMessageBusSensor_P', cg.Parented.template(GDoor))
-GDoorBusEvent_P = gdoor_esphome_ns.class_('GDoorBusEvent_P', cg.Parented.template(GDoor))
+data_validation_list = []
 
-CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(GDoor),
-    cv.Optional(CONF_RX_PIN, default='GPIO22'): cv.gpio_input_pin_schema,
-    cv.Optional(CONF_SENSITIVITY, default=1.65): cv.positive_float.validators(cv.Range(min=1.3, max=1.65))
-})
+def data(value):
+  value = cv.string_strict(value).lower()
+  if re.match("^([0-9a-f]{2})+$", value):
+    if value in data_validation_list:
+      raise cv.Invalid('data field must be a unique frame value')
+    else:
+      data_validation_list.append(value)
+      return value
+  else:
+    raise cv.Invalid('data must be a hex byte representation, see https://gdoor-org.github.io/documentation/protocol.html for details')
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
-    cg.add(var.set_rx_pin(config[CONF_RX_PIN]))
-    cg.add(var.set_rx_sensitivity(config[CONF_SENSITIVITY]))
